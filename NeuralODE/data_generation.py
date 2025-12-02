2529b3a75b1c47946f52602ac90f14d974e442a6
Jianshu-Hu <jianshuhu98@gmail.com>
2025-12-02 12:05:39 +0800

    update data_generation.py in NeuralODE to speed up the code and include positions of the needle end in the obs

--- NeuralODE/data_generation.py
+++ NeuralODE/data_generation.py
@@ -18,9 +18,11 @@ Supported tasks:
 NOTE: This code only supports tasks that use a single PSM (Patient Side Manipulator).
 """
 
-import numpy as np
 import argparse
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+import numpy as np
 from tqdm import tqdm
 
 TASKS = [
@@ -56,6 +58,11 @@ data = np.load(
     f'../SurRoL/surrol/data/demo/data_{args.task}_random_100.npz', 
     allow_pickle=True
 )
+obs_data = data['obs']  # materialize to avoid multi-threaded zip reads
+acs_pos = data['acs'][:, :, 0:3]       # [demo, timestep, [dx, dy, dz]]
+acs_orn = data['acs'][:, :, 3:]  # [demo, timestep, [d_yaw, jaw]]
+num_demo, num_timestep = data['obs'].shape
+data.close()
 
 # Determine task domain
 if args.task not in TASKS:
@@ -63,52 +70,52 @@ if args.task not in TASKS:
     print(TASKS)
     exit(0)
 
-num_demo, num_timestep = data['obs'].shape
 
-if args.task in ['NeedlePick-v1', 'NeedlePick-v2']:
-    '''
-    Observation len = 33
-    obs[0:7]: PSM's pose (3 position, 3 euler angle, 1 jaw status)
-    obs[7:10]: Object's position (Needle's base link position) (I don't think it is useful because it is not exactly on the needle)
-    obs[10:13]: Object's relative position (obs[7:10] - obs[0:3])
-    obs[13:16]: Position of center of the needle (the usual pick up place)
-    obs[16:19]: Orientation of center of the needle (Euler angles)
-    obs[19:22]: Position of left end of the needle
-    obs[22:25]: Orientation of left end of the needle (Euler angles)
-    obs[25:28]: Position of right end of the needle
-    obs[28:31]: Orientation of right end of the needle (Euler angles)
-    '''
-
-    # TODO: I have listed all the info from obs and generate the SurRoL demo data. 
-    #       Choose what you need for ODE training.
-    #       Make sure you change the dimension of obs_pos, obs_orn, acs_pos, acs_orn accordingly.
-    ...
+'''
+For ['NeedlePick-v0', 'NeedlePick-v1', 'NeedlePick-v2']:
+Observation len = 33
+obs[0:7]: PSM's pose (3 position, 3 euler angle, 1 jaw status)
+obs[7:10]: Object's position (Needle's base link position) (I don't think it is useful because it is not exactly on the needle)
+obs[10:13]: Object's relative position (obs[7:10] - obs[0:3])
+obs[13:16]: Position of center of the needle (the usual pick up place)
+obs[16:19]: Orientation of center of the needle (Euler angles)
+obs[19:22]: Position of left end of the needle
+obs[22:25]: Orientation of left end of the needle (Euler angles)
+obs[25:28]: Position of right end of the needle
+obs[28:31]: Orientation of right end of the needle (Euler angles)
+'''
 
 # Initialize arrays for single PSM tasks
 obs_pos = np.zeros((num_demo, num_timestep, 3))  # [demo, timestep, xyz]
-acs_pos = data['acs'][:, :, 0:3]       # [demo, timestep, [dx, dy, dz]]
 obs_orn = np.zeros((num_demo, num_timestep, 4))  # [demo, timestep, [roll, pitch, yaw, jaw_angle]]
-acs_orn = data['acs'][:, :, 3:]  # [demo, timestep, [d_yaw, jaw]]
 
-obj_pos = np.zeros((num_demo, num_timestep, 3))  # [demo, timestep, xyz]
+obj_pos = np.zeros((num_demo, num_timestep, 6))  # [demo, timestep, [leftxyz, rightxyz]]
 
-# Extract position data from observations
-for demo_idx in tqdm(range(num_demo), desc='demos', unit='demo'):
+# Extract data from observations using threads for speed
+
+def _process_demo(demo_idx: int):
+    demo_obs_pos = np.zeros((num_timestep, 3))
+    demo_obs_orn = np.zeros((num_timestep, 4))
+    demo_obj_pos = np.zeros((num_timestep, 6))
+
     for timestep_idx in range(num_timestep):
-        # Extract x, y, z coordinates from observation
-        obs_pos[demo_idx, timestep_idx, :] = \
-            data['obs'][demo_idx][timestep_idx]['observation'][0:3]
+        obs = obs_data[demo_idx][timestep_idx]['observation']
+        demo_obs_pos[timestep_idx, :] = obs[0:3]
+        demo_obs_orn[timestep_idx, :] = obs[3:7]
 
-        # TODO: Index in specified order
-        # data['obs'][demo_idx][timestep_idx]['observation'][[0, 1, 2, 7, 8, 9, 13, 14, 15, ...]]
-        
-        # Extract w, x, y, z coordinates from observation
-        obs_orn[demo_idx, timestep_idx, :] = \
-            data['obs'][demo_idx][timestep_idx]['observation'][3:7]
+        if args.task in ['NeedlePick-v0', 'NeedlePick-v1', 'NeedlePick-v2']:
+            demo_obj_pos[timestep_idx, :] = obs[[19, 20, 21, 25, 26, 27]]
 
-        # Extract x, y, z coordinates from observation
-        obj_pos[demo_idx, timestep_idx, :] = \
-            data['obs'][demo_idx][timestep_idx]['observation'][7:10]
+    return demo_idx, demo_obs_pos, demo_obs_orn, demo_obj_pos
+
+
+with ThreadPoolExecutor() as executor:
+    futures = {executor.submit(_process_demo, demo_idx): demo_idx for demo_idx in range(num_demo)}
+    for future in tqdm(as_completed(futures), total=num_demo, desc='demos', unit='demo'):
+        demo_idx, demo_obs_pos, demo_obs_orn, demo_obj_pos = future.result()
+        obs_pos[demo_idx] = demo_obs_pos
+        obs_orn[demo_idx] = demo_obs_orn
+        obj_pos[demo_idx] = demo_obj_pos
 
 # Save processed data to files
 np.save(f'data/{args.task}/obs_pos.npy', obs_pos)
